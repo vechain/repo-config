907af9fcf522284ae7a513c0477ebfe4df2f5324
Davide Carpini <carpini.davide@gmail.com>
2023-10-26 15:36:37 +0200

    config

---
+++ src/eslint/react.js
@@ -0,0 +1,41 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+module.exports = {
+  extends: [
+    "@vercel/style-guide/eslint/browser",
+    "@vercel/style-guide/eslint/typescript",
+    "@vercel/style-guide/eslint/react",
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
+  parser: "@typescript-eslint/parser",
+  globals: {
+    JSX: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js", "**/*.css"],
+  // add rules configurations here
+  rules: {
+    "import/no-default-export": "off",
+    "unicorn/filename-case": "off",
+    "react-hooks/exhaustive-deps": "error",
+    "react/function-component-definition": [
+      "error",
+      {
+        namedComponents: "arrow-function",
+        unnamedComponents: "arrow-function",
+      },
+    ],
+    "prefer-arrow-callback": ["error", { allowNamedFunctions: true }],
+    "func-style": ["error", "expression", { allowArrowFunctions: true }],
+  },
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const Prettier = require("./prettier.json");
+const EslintReactNative = require("./eslint/react-native.js");
+const EslintReact = require("./eslint/react.js");
+const EslintLibrary = require("./eslint/library.js");
+const TsConfigBase = require("./tsconfig/base.json");
+const TsConfigReact = require("./tsconfig/react.json");
+const TsConfigLibrary = require("./tsconfig/library.json");
+
+module.exports = {
+  Prettier,
+  EslintReactNative,
+  EslintReact,
+  EslintLibrary,
+  TsConfigBase,
+  TsConfigReact,
+  TsConfigLibrary,
+};
