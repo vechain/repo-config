41d69124b10f6e0adf2c878294b91eec4b4b5027
Darren Kelly <darren.kelly@vechain.org>
2023-10-27 11:56:26 +0100

    add vue to index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ const Prettier = require("./prettier.json");
 const EslintReactNative = require("./eslint/react-native.js");
 const EslintReact = require("./eslint/react.js");
 const EslintLibrary = require("./eslint/library.js");
+const EslintVue = require("./eslint/vue.js");
 const TsConfigBase = require("./tsconfig/base.json");
 const TsConfigReact = require("./tsconfig/react.json");
 const TsConfigReactLibrary = require("./tsconfig/react-library.json");
+const TsConfigVue = require("./tsconfig/vue.json");
+
 
 module.exports = {
   Prettier,
   EslintReactNative,
   EslintReact,
   EslintLibrary,
+  EslintVue,
   TsConfigBase,
   TsConfigReact,
   TsConfigReactLibrary,
+  TsConfigVue,
 };
